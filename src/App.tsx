481e1b3b63ccfffbe495774c20703822d1e8b9a3
Chris Tam <tamkachun2908@gmail.com>
2021-06-06 19:20:05 +0800

    feat: markdown support tailwindcss title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const markdowntext: string = `
+  # testing title 6xl
+  ## testing title 5xl
+  ### testing title 4xl
+  #### testing title 3xl
+  ##### testing title 2xl
+  ###### testing title xl
+
+  normal text
+  `;
   return (
-    <div className="bg-gray-500 text-white">Testing</div> 
+    <div>
+      <ReactMarkdown components={{
+        h1: ({node, ...props}) => {return <div className="text-6xl" {...props}></div>},
+        h2: ({node, ...props}) => {return <div className="text-5xl" {...props}></div>},
+        h3: ({node, ...props}) => {return <div className="text-4xl" {...props}></div>},
+        h4: ({node, ...props}) => {return <div className="text-3xl" {...props}></div>},
+        h5: ({node, ...props}) => {return <div className="text-2xl" {...props}></div>},
+        h6: ({node, ...props}) => {return <div className="text-xl" {...props}></div>}  
+      }}>
+        {markdowntext}
+      </ReactMarkdown>
+    </div>
   );
 }
 
