a46e32721a8c9baa80eb29f19b77dec6ced52d7f
Chris Tam <tamkachun2908@gmail.com>
2021-06-19 14:45:00 +0800

    feat: prevent tab

--- src/MarkdownInput.tsx
+++ src/MarkdownInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react'	
+import React, { useRef } from 'react'	
 
 export interface markdownInputProps {
 	markdownText: string,
@@ -7,6 +7,14 @@ export interface markdownInputProps {
 
 export const MarkdownInput = ({markdownText, setMarkdownText}: markdownInputProps): JSX.Element => {
 
+	const markdownTextarea = useRef<HTMLTextAreaElement>(null)
+
+	markdownTextarea.current?.addEventListener('keydown', function(e: KeyboardEvent) {
+		if(e.key == 'Tab'){
+			e.preventDefault()
+		}
+	})
+
 	const updateMarkdown = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
 		setMarkdownText(event.target.value)
 	}
@@ -14,7 +22,7 @@ export const MarkdownInput = ({markdownText, setMarkdownText}: markdownInputProp
 	return (
 		<div className="h-5/6 min-w-full max-w-full w-full">
 			<span className="ml-2 text-2xl font-semibold">Markdown text:</span>
-			<textarea className="resize-none w-full h-full rounded-md m-2 border border-gray-500" value={ markdownText } onChange={ updateMarkdown } ></textarea>
+			<textarea ref={markdownTextarea} className="p-2 resize-none w-full h-full rounded-md m-2 border border-gray-500" value={ markdownText } onChange={ updateMarkdown } ></textarea>
 		</div>	
 	)
 }
